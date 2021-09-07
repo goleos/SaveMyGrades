ea858cc36e2945db93400d9fb0b9b18a98e42f10
LeonidGoldberg <leonid.goldberg@outlook.com>
2021-09-07 11:48:59 +0300

    Initial Commit

---
+++ SaveMyGrades/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SaveMyGrades
+//
+//  Created by Leonid Goldberg on 07/09/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
